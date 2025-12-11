db8278b7bd73f8ed464d036f6a1273b00a34810e
Ramzan <muhammadramzan3043@gmail.com>
2025-12-11 14:27:32 +0500

    ramzan

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Header from "../components/header";
+import TopBar from "../components/topBar";
+
 export default function Home() {
     return (
-        <h1>Home </h1>
+        <>
+            <TopBar />
+            <Header />
+        </>
     )
 }
